2da64b975bfaa8e73aa656788b4fd29dd945ad03
renan823 <renan.trofino@gmail.com>
2024-02-10 19:07:21 -0300

    data types

---
+++ 02-DataTypes/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+    int age = 14;
+    float money = 15;
+    char favoriteLetter = 'c';
+
+    printf("I'm %d years old, and I have %f dollars. My favorite letter is %c\n", age, money, favoriteLetter);
+
+    return 0;
+}
